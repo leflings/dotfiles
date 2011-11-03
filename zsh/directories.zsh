4fcc296a0eeaff83d452d3d643145e6a9225bc4e
Flemming Madsen <mail@leflings.dk>
2011-11-03 23:26:10 +0100

    Added some plugins. Made tmp/ directory under vim to hold undo, backup and swap files

---
+++ zsh/directories.zsh
@@ -0,0 +1,15 @@
+# bunch of aliases for easier traversing directories
+alias l1='tree --dirsfirst -ChFL 1'
+alias l2='tree --dirsfirst -ChFL 2'
+alias l3='tree --dirsfirst -ChFL 3'
+
+alias ll1='tree --dirsfirst -ChFupDaL 1'
+alias ll2='tree --dirsfirst -ChFupDaL 2'
+alias ll3='tree --dirsfirst -ChFupDaL 3'
+
+alias l=l1
+alias ll=ll1
+
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'

a08b7b64d4d8d0e7cf9a36f3a22b12b9bd174f76
Flemming Madsen <mail@leflings.dk>
2012-09-05 13:14:32 +0200

    Updated aliases to new semester. Adds new Vim plugin

--- system/folders.zsh
+++ system/folders.zsh
@@ -1,4 +1,4 @@
 # shortcuts for folders i use often
 alias 'gts=cd ~/Dropbox/DTU/1.\ Semester/Indledende\ programmering/javasrc/'
 alias 'gtj=cd ~/code/Java/'
-alias 'gtd=cd ~/Dropbox/DTU/2.\ Semester/'
+alias 'gtd=cd ~/Dropbox/DTU/3.\ Semester/'

367fe6a9e7cab1fac381c8def22b840b49a19470
Flemming Madsen <mail@leflings.dk>
2013-02-17 13:58:07 +0100

    Cleanup in debian branch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,23 +7,15 @@
 #
 # I'm hardcoding it to an installed rvm (using rvm's `rvm 1.8.7,ruby /hub/path`
 # syntax is way too slow). It should work fine for those without rvm, though.
-if [[ -s $HOME/.rvm/scripts/rvm ]]
-then
-  if $(which hub &> /dev/null) && [[ -s $HOME/.rvm/rubies/ruby-1.8.7-p334 ]]
-  then
-    alias git='$HOME/.rvm/rubies/ruby-1.8.7-p334/bin/ruby `which hub`'
-  else
-  fi
-fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+#alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
+#alias gp='git push origin HEAD'
+#alias gd='git diff'
+#alias gc='git commit'
+#alias gca='git commit -a'
+#alias gco='git checkout'
+#alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+#alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
--- system/folders.zsh
+++
@@ -1,4 +0,0 @@
-# shortcuts for folders i use often
-alias 'gts=cd ~/Dropbox/DTU/1.\ Semester/Indledende\ programmering/javasrc/'
-alias 'gtj=cd ~/code/Java/'
-alias 'gtd=cd ~/Dropbox/DTU/3.\ Semester/'

42c1033a43a62d946905b33d2c033952e66506cb
Flemming Madsen <mail@leflings.dk>
2014-09-28 17:01:49 +0200

    Cleanup of PATH configuration. Add some VIM plugins

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+if [ -x /usr/libexec/path_helper ]; then
+	PATH=""
+	eval `/usr/libexec/path_helper -s`
+fi
